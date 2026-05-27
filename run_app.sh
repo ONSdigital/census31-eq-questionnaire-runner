635a4c7d1ed8c35da9fb10981feb6983d29d2c8b
Mark Price <mark.price@ons.gov.uk>
2026-05-27 11:02:20 +0100

    reinstating final run_command inadvertantly removed for docker startup

--- run_app.sh
+++ run_app.sh
@@ -3,9 +3,9 @@
 set -e
 
 if [ "$WEB_SERVER_TYPE" = "gunicorn-async" ]; then
-    run_command="gunicorn application:application --worker-class gevent"
+    run_command="gunicorn application:application --worker-class gevent --timeout 0"
 elif [ "$WEB_SERVER_TYPE" = "gunicorn-threads" ]; then
-    run_command="gunicorn application:application --worker-class gthread"
+    run_command="gunicorn application:application --worker-class gthread --timeout 0"
 elif [ "$WEB_SERVER_TYPE" = "uwsgi" ]; then
     run_command="uwsgi uwsgi.ini --workers ${WEB_SERVER_WORKERS} --http-keepalive=${HTTP_KEEP_ALIVE}"
 elif [ "$WEB_SERVER_TYPE" = "uwsgi-threads" ]; then
@@ -13,3 +13,5 @@ elif [ "$WEB_SERVER_TYPE" = "uwsgi-threads" ]; then
 elif [ "$WEB_SERVER_TYPE" = "uwsgi-async" ]; then
     run_command="uwsgi uwsgi.ini --module patched:application --workers ${WEB_SERVER_WORKERS} --gevent ${WEB_SERVER_UWSGI_ASYNC_CORES} --http-keepalive=${HTTP_KEEP_ALIVE}"
 fi
+
+eval "$run_command"
