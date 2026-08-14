c1d3c16b3bf887770d9ae052f8426148bc69b9a0
R McCarthy <42928680+rmccar@users.noreply.github.com>
2026-08-14 10:57:59 +0100

    remove validator branch

--- scripts/run_validator.sh
+++ scripts/run_validator.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-tag=update-list-of-valid-themes
+tag=latest
 TAG=${tag} docker compose -f docker-compose-schema-validator.yml pull
 TAG=${tag} docker compose -f docker-compose-schema-validator.yml up -d
