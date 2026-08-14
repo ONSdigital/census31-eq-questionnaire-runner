cec84afb76c380cc7edb80fe355edd535ab59c38
R McCarthy <42928680+rmccar@users.noreply.github.com>
2026-08-14 09:14:19 +0100

    update ni name

--- app/survey_config/survey_type.py
+++ app/survey_config/survey_type.py
@@ -4,5 +4,5 @@ from enum import Enum
 class SurveyType(Enum):
     DEFAULT = "default"
     CENSUS = "census"
-    NI_CENSUS = "ni-census"
+    NI_CENSUS = "nisra-census"
     NRS_CENSUS = "nrs-census"
