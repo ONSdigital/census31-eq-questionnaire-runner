e889f107759f8cb478fdd8c08954fb777dc26b20
R McCarthy <42928680+rmccar@users.noreply.github.com>
2026-08-17 08:17:49 +0100

    update default to use census config

--- app/routes/errors.py
+++ app/routes/errors.py
@@ -14,7 +14,7 @@ from app.authentication.no_token_exception import NoTokenException
 from app.globals import get_metadata
 from app.helpers.language_helper import handle_language
 from app.helpers.template_helpers import get_survey_config, render_template
-from app.settings import ACCOUNT_SERVICE_BASE_URL, ACCOUNT_SERVICE_BASE_URL_CENSUS
+from app.settings import ACCOUNT_SERVICE_BASE_URL_CENSUS
 from app.submitter.previously_submitted_exception import PreviouslySubmittedException
 from app.submitter.submission_failed import SubmissionFailedException
 from app.survey_config.survey_type import SurveyType
@@ -47,7 +47,7 @@ def log_exception(exception: Exception, status_code: int) -> None:
 def _render_error_page(status_code: int, template: str | int | None = None, **kwargs: Any) -> tuple[str, int]:
     handle_language()
     census_survey_config = get_survey_config(theme=SurveyType.CENSUS, base_url=ACCOUNT_SERVICE_BASE_URL_CENSUS)
-    default_survey_config = get_survey_config(theme=SurveyType.DEFAULT, base_url=ACCOUNT_SERVICE_BASE_URL)
+    default_survey_config = get_survey_config(theme=SurveyType.DEFAULT, base_url=ACCOUNT_SERVICE_BASE_URL_CENSUS)
 
     census_logout_url = census_survey_config.account_service_log_out_url
     census_contact_us_url = census_survey_config.contact_us_url
