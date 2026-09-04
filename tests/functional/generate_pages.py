78cd28a329b79a1671bf48481e80b46c16168436
R McCarthy <42928680+rmccar@users.noreply.github.com>
2026-09-04 12:41:17 +0100

    ignore some rules

--- tests/functional/generate_pages.py
+++ tests/functional/generate_pages.py
@@ -730,9 +730,9 @@ def build_and_get_base_page_context(
     return context
 
 
-def process_block(
+def process_block(  # noqa: C901, PLR0912, PLR0915
     block, dir_out, schema_data, spec_file, relative_require="..", page_filename=None
-):  # noqa: C901, PLR0912, PLR0915
+):
     logger.debug("Processing Block: %s", block["id"])
 
     if not page_filename:
