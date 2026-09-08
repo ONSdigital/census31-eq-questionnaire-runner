07e52e00bf0d99476bd0bd9b9305c8e970bca6cf
R McCarthy <42928680+rmccar@users.noreply.github.com>
2026-09-08 10:06:39 +0100

    fix indentation

--- tests/integration/test_previously_submitted.py
+++ tests/integration/test_previously_submitted.py
@@ -4,18 +4,18 @@ from tests.integration.questionnaire import THANK_YOU_URL_PATH
 
 class TestPreviouslySubmitted(IntegrationTestCase):
     setting_overrides = {}
-        # Given I complete the questionnaire and submit
-        self.launchSurveyV2(schema_name="test_textfield")
-        self.post()
-        self.post()
-        self.assertInUrl(THANK_YOU_URL_PATH)
+    # Given I complete the questionnaire and submit
+    self.launchSurveyV2(schema_name="test_textfield")
+    self.post()
+    self.post()
+    self.assertInUrl(THANK_YOU_URL_PATH)
 
-        # When I try to return to a page in the questionnaire
-        self.get("/questionnaire/submit/")
+    # When I try to return to a page in the questionnaire
+    self.get("/questionnaire/submit/")
 
-        # Then I should receive a 401 unauthorised code and be redirected to the submission complete page
-        self.assertStatusUnauthorised()
-        self.assertEqualPageTitle("Submission Complete - Test Textfield")
-        self.assertInBody("This page is no longer available")
-        self.assertInBody("Your survey has been submitted")
-        self.assertInBody(f'<a href="{THANK_YOU_URL_PATH}">Return to previous page</a>')
+    # Then I should receive a 401 unauthorised code and be redirected to the submission complete page
+    self.assertStatusUnauthorised()
+    self.assertEqualPageTitle("Submission Complete - Test Textfield")
+    self.assertInBody("This page is no longer available")
+    self.assertInBody("Your survey has been submitted")
+    self.assertInBody(f'<a href="{THANK_YOU_URL_PATH}">Return to previous page</a>')
