7315d3d55f7c1da0a5c6c977accf81e3ca575d37
Andrew Maddaford <1754822+ajmaddaford@users.noreply.github.com>
2026-09-18 14:06:12 +0100

    CTE-296: Switch to multistage build distroless container (#139)

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -2,8 +2,14 @@ import os
 
 import gunicorn
 
+if os.getenv("WEB_SERVER_TYPE") == "gunicorn-async":
+    worker_class = "gevent"
+elif os.getenv("WEB_SERVER_TYPE") == "gunicorn-threads":
+    worker_class = "gthread"
+
 workers = os.getenv("WEB_SERVER_WORKERS")
 threads = os.getenv("WEB_SERVER_THREADS")
 keepalive = os.getenv("HTTP_KEEP_ALIVE")
+timeout = 0
 bind = "0.0.0.0:5000"
 gunicorn.SERVER_SOFTWARE = "None"
